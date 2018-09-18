ecd72981ff2a60f887ac7bb604c0fbceb73042de
Zack <36819949+pbrianz@users.noreply.github.com>
2018-09-17 19:35:07 -0700

    Create truffles.rb

---
+++ truffles.rb
@@ -0,0 +1,14 @@
+class CLI
+  # Create the class.
+  f = this.methods
+  
+  while true do
+    in_raw = gets.chomp
+    in = in_raw.split(" ")
+    for method in f do
+      if in[0] == method then
+        send(this[method], in.drop(1))
+      end
+    end
+  end
+end
