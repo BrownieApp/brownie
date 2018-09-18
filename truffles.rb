50bdd2e6c98ecfbdcc938f2125137104a5ea1e97
Zack <36819949+pbrianz@users.noreply.github.com>
2018-09-17 21:11:42 -0700

    Update truffles.rb

--- truffles.rb
+++ truffles.rb
@@ -6,17 +6,26 @@ class CLI
     in_raw = gets.chomp
     inc = in_raw.split(" ")
     for method in f do
-      if inc.include? method then
-        if in[0] == method then
-          send(this[method], inc.drop(1))
-        end
-      else
+      if ARGV.length == 0 then
+        if inc.include? method then
+          if in[0] == method then
+            send(this[method], inc.drop(1))
+          end
+        else
           puts "Command not found. \n List of commands \n #{f.join('\n')}"
+        end
+        else
+          if inc.include? method then
+          if ARGV[0] == method then
+            send(this[ARGV[0]], inc.drop(1))
+          end
+        else
+          puts "Command not found. \n List of commands \n #{f.join('\n')}"
+        end
       end
     end
   end
 end
-
 def input(label)
   puts label
   return gets.chomp
