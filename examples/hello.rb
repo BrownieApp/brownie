24b9ffd7ccc8ecb9f92eb429ca6ff717f2f5eb94
Zack <36819949+pbrianz@users.noreply.github.com>
2018-09-18 14:39:40 -0700

    Delete hello.rb

--- examples/hello.rb
+++
@@ -1,8 +0,0 @@
-require "truffles" # If the gem is installed.  To use this install the gem or move the truffles.rb file to the examples library and comment this line, then uncomment the second line.
-# require "./truffles"
-
-class CLI < truffles.CLI
-  def hello(args)
-    truffles.print "Hello, #{args[0]}!"
-  end
-end
