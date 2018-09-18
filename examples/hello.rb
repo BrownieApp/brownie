c6863b53f636d839223c9c43f19c0503b2daac6a
Zack <36819949+pbrianz@users.noreply.github.com>
2018-09-17 19:37:37 -0700

    Create hello.rb

---
+++ examples/hello.rb
@@ -0,0 +1,8 @@
+require "truffles" # If the gem is installed.  To use this install the gem or move the truffles.rb file to the examples library and comment this line, then uncomment the second line.
+# require "./truffles"
+
+class CLI < truffles.CLI
+  def hello(args)
+    puts "Hello, #{args[0]}!"
+  end
+end
